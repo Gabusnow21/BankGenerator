d2931ec04ee9230446e1d87eea210218e118e439
gabus2194 <dg22023@ues.edu.sv>
2026-05-30 08:46:41 -0600

    Add: database, themes, utils

---
+++ app/src/main/java/com/gabusdev/dev/quizbank/data/database/AppDatabase.java
@@ -0,0 +1,25 @@
+package com.gabusdev.dev.quizbank.data.database;
+
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import com.gabusdev.dev.quizbank.data.models.DocenteEntity;
+
+@Database(entities = {DocenteEntity.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+    private static volatile AppDatabase INSTANCE;
+
+    public static AppDatabase getInstance(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "quizbank_db")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
