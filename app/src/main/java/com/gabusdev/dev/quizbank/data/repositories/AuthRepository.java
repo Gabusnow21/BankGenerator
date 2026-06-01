b9aa7811f57b4b447163c7cf33aeb45728b878c1
gabus2194 <dg22023@ues.edu.sv>
2026-06-01 15:35:06 -0600

    Fix: Autenticacion con google drive

--- app/src/main/java/com/gabusdev/dev/quizbank/data/repositories/AuthRepository.java
+++ app/src/main/java/com/gabusdev/dev/quizbank/data/repositories/AuthRepository.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.Scope;
+import com.google.api.services.drive.DriveScopes;
 
 public class AuthRepository {
     private final GoogleSignInClient googleSignInClient;
@@ -11,6 +13,7 @@ public class AuthRepository {
     public AuthRepository(Context context) {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
+                .requestScopes(new Scope(DriveScopes.DRIVE_FILE))
                 .build();
         this.googleSignInClient = GoogleSignIn.getClient(context, gso);
     }
